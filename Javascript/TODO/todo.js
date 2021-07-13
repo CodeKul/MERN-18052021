6ea2cb10df0ba9a9e1416f3288d13196cefc1839
chaudharisucheta <chaudharisucheta@gmail.com>
2021-07-13 21:42:05 +0530

    DOM

--- Javascript/TODO/todo.js
+++ Javascript/TODO/todo.js
@@ -1,13 +1,20 @@
 let todoInput = document.getElementById("todoInput");
 let addBtn = document.getElementById("addBtn");
 let todoList = document.getElementById("todo-tasks");
-
+let completedList = document.getElementById("completed");
+let clear = document.createElement("button");
+clear.innerText = "Clear ALL";
+completedList.append(clear);
 addBtn.addEventListener("click", addTodo);
 
 function addTodo() {
   let todoItem = document.createElement("li");
   let todoText = document.createElement("label");
   let delBtn = document.createElement("button");
+  let completedBtn = document.createElement("button");
+
+  completedBtn.innerText = "Done";
+  completedBtn.addEventListener("click", completedTodo);
 
   delBtn.innerText = "Delete";
   todoText.innerText = todoInput.value;
@@ -16,12 +23,27 @@ function addTodo() {
   //appending label,button to LI
   todoItem.append(todoText);
   todoItem.append(delBtn);
-
+  todoItem.append(completedBtn);
   //appending LI to UL
   todoList.append(todoItem);
   todoInput.value = "";
 }
 
 function deleteTodo(e) {
-  console.log(e.target);
+  let item = e.target;
+  let tobeRemoved = item.parentElement;
+  tobeRemoved.remove();
 }
+
+function completedTodo(e) {
+  let completedItem = e.target;
+  let tobeCompleted = completedItem.parentElement;
+  completedList.append(tobeCompleted);
+  tobeCompleted.style.textDecoration = "line-through";
+  clear.addEventListener("click", () => {
+    tobeCompleted.style.display = "none";
+  });
+}
+//Edit functionality for todoItem
+//Create edit button
+//add input field so that the todoText can be edited
