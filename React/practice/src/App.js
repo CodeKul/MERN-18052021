4b7122e6b76a28493d04db3771cb3335f365a781
sucheta-codekul <sucheta.codekul@gmail.com>
2021-08-28 11:03:07 +0530

    react-project

---
+++ React/practice/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+      ets up your development environment so that you can use the latest JavaScript features, provides a nice developer experience, and optimizes your app for production. You’ll need to
+      </p>
+    </div>
+  );
+}
+
+export default App;
